d517ded15d4c6cf02ef0ab9754392973c671f796
Ben Bellerose <belleroseb@hotmail.com>
2022-02-22 21:35:06 -0500

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -35,9 +35,8 @@ class Sudoku:
                 break
         return self.board
     
-    def mask_board(self):
+    def mask_board(self, i_choice = [1, 3]):
         p_nums = [i for i in range(self.size)]
-        i_choice = [8, 9]
         for y in range(self.size):
             i_amt = choices(i_choice, k=1)[0]
             i_pos = choices(p_nums, k=i_amt)
@@ -46,48 +45,37 @@ class Sudoku:
 game = Sudoku()
 
 solution = game.board
-game.mask_board()
+game.mask_board([1, 4])
 m_board = game.board
-
-print(np.array(m_board))
 
 def search_tree(p_moves, board, x_hold, y_hold, s_hold, p_nums, square=3, y=0, x=0):
     if x == 9:
         y += 1
         x = 0
-    if (x < len(x_hold) and y < len(y_hold) and x + ((y // square) * square) < len(s_hold)):
-        #print(p_moves[y][x])
+    if (y < len(x_hold) and x < len(y_hold) and (x // square) + ((y // square) * square) < len(s_hold)):
         if board[y][x] == '.':
             for p in p_moves[y][x]:
-                print('---')
-                print(x, y, x + ((y // square) * square))
-                print(p)
-                print(x_hold[x])
-                print(y_hold[y])
-                print(s_hold[x + ((y // square) * square)])
-                print(np.array(board))
-                if p not in x_hold[x] and p not in y_hold[y] and p not in s_hold[x + ((y // square) * square)]:
-                    print(f'guess')
+                if p not in x_hold[y] and p not in y_hold[x] and p not in s_hold[(x // square) + ((y // square) * square)]:
                     temp_board = deepcopy(board)
                     temp_x_hold = deepcopy(x_hold)
                     temp_y_hold = deepcopy(y_hold)
                     temp_s_hold = deepcopy(s_hold)
                     
                     temp_board[y][x] = p
-                    temp_x_hold[x].append(p)
-                    temp_y_hold[y].append(p)
+                    temp_x_hold[y].append(p)
+                    temp_y_hold[x].append(p)
                     temp_s_hold[(x // square) + ((y // square) * square)].append(p)
-                    
                     temp_board = search_tree(p_moves, temp_board, temp_x_hold, temp_y_hold, temp_s_hold, p_nums=p_nums, square=square, y=y, x=x+1)
                     if temp_board is not None and True not in [True for r in temp_board if '.' in r]:
                         return temp_board
-                else:
-                    print(f'bad')
         else:
             temp_board = search_tree(p_moves, board, x_hold, y_hold, s_hold, p_nums=p_nums, square=square, y=y, x=x+1)
             if temp_board is not None and True not in [True for r in temp_board if '.' in r]:
                 return temp_board
-    return None
+    if board is not None and True not in [True for r in board if '.' in r]:
+        return board
+    else:
+        return None
     
 
 def solve_game(board, square=3):
@@ -104,17 +92,25 @@ def solve_game(board, square=3):
             for x in range(len(board[y])):
                 if l == 0:
                     if board[y][x] != '.':
-                        x_hold[x].append(board[y][x])
-                        y_hold[y].append(board[y][x])
+                        x_hold[y].append(board[y][x])
+                        y_hold[x].append(board[y][x])
                         s_hold[(x // square) + ((y // square) * square)].append(board[y][x])
                 else:
                     if board[y][x] == '.':
-                        pos_hold = [n for n in p_nums if n not in s_hold[(x // 3) + ((y // 3) * 3)] and n not in y_hold[y] and n not in x_hold[x]]
-                        p_board[-1].append(pos_hold)
+                        pos_hold = [n for n in p_nums if n not in s_hold[(x // 3) + ((y // 3) * 3)] and n not in y_hold[x] and n not in x_hold[y]]
+                        if len(pos_hold) == 1:
+                            board[y][x] = pos_hold[0]
+                            x_hold[y].append(board[y][x])
+                            y_hold[x].append(board[y][x])
+                            s_hold[(x // square) + ((y // square) * square)].append(board[y][x])
+                            p_board[-1].append([])
+                        else:
+                            p_board[-1].append(pos_hold)
                     else:
-                        p_board[-1].append([board[y][x]])
-    print(p_board)
+                        p_board[-1].append([])
     board = search_tree(p_board, board, x_hold, y_hold, s_hold, p_nums=p_nums, square=square, y=0, x=0)
-    print(np.array(board))
-    
-solve_game(m_board)
+    return board
+
+print(np.array(m_board))
+print()
+solution = solve_game(m_board)
